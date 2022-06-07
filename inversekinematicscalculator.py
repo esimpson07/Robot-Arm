d8cb8a746981122bb7f120884beea2a478eabc21
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-06-06 20:51:20 -0400

    Update inversekinematicscalculator.py
    Long overdue for a commit, been working on this code for a while but only committed it locally for a while. Added: Fixes for all of the angle mess-ups, as well as smoothing for the movements, so it is not jerky.

--- inversekinematicscalculator.py
+++ inversekinematicscalculator.py
@@ -1,77 +1,127 @@
 import math
+from time import sleep
+from adafruit_servokit import ServoKit
 
-#definitions
-arm1 = 100
-arm2 = 80
-clawLength = 30
-baseHeight = 30
+kit = ServoKit(channels=16)
 
-#variables
+#on servos 0 and 4, 0-180 is not 100% safe, since that is the maximum range of the servo
+#on servo 15, full open -> closed is 30 -> 85
+kit.servo[0].angle = 90
+kit.servo[1].angle = 90
+kit.servo[2].angle = 45
+kit.servo[13].angle = 90
+kit.servo[14].angle = 90
+kit.servo[15].angle = 30
+
+arm1 = 103
+arm2 = 97
+pdist = 140
 dist = 100
-A1 = 0
-A2 = 0
+clawLength = 150
+baseHeight = 107
+x = 200
+y = 270
+z = 150
+tx = -200
+ty = 280
+tz = 107
+steps = 100 #~40 steps will be executed a second, tells how many segments to divide movement into
 A = 0
-B1 = 0
-B2 = 0
 B = 0
 C = 0
-x = 0
-y = 0
-z = 0
+R = 0
 
-x = 30
-y = 70
-z = 40
+def iloc(a,b,c): #inverse law of cosines (law of arccosines)
+    try:
+        return(round(math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b))),2))
+    except ValueError:
+        print("inverse law of cosines unable to calculate!!!")
+        return(0)
 
-#Angle R = rotation servo angle
-#Angle A = first lift servo angle
-#Angle C = second lift servo angle
-#Angle B = third lift servo angle
-#Angle D = fourth servo, claw rotation angle
-#Angle E = final servo, claw open/close angle
+def smooth(): #servo movement smoothening
+    global x
+    global y
+    global z
+    global tx
+    global ty
+    global tz
+    global steps
+    if(steps > 0):
+        xd = x - tx
+        yd = y - ty
+        zd = z - tz
+        x -= (xd / steps)
+        y -= (yd / steps)
+        z -= (zd / steps)
+        steps -= 1 
 
-''' Array will be cartesian coordinates '''
-''' # # # # # # # # # # # # # # # # # #
-    # # # # # # # # # # # # # # # # # #
-  ^ # # # # # # # # # # # # # # # # # #
-  | # # # # # # # # # # # # # # # # # #
-  Y # # # # # # # # # # # # # # # # # #
-  | # # # # # # # # # # # # # # # # # #
-  v # # # # # # # # # # # # # # # # # #
-    # # # # # # # # # # # # # # # # # #
-    # # # # # # # # # # # # # # # # # #
-    # # # # # # # # Claw# # # # # # # #
-                < - - X - - >
-'''
+def cv(): #check values
+    global A
+    global B
+    global C
+    global R
+    if(A > 180):
+        A = 180
+        print("A capping!!!")
+    elif(A < 0):
+        A = 0
+        print("A flooring!!!")
+    if(B > 180):
+        B = 180
+        print("B capping!!!")
+    elif(B < 0):
+        B = 0
+        print("B flooring!!!")
+    if(C > 225):
+        C = 225
+        print("C capping!!!")
+    elif(C < 45):
+        C = 45
+        print("C flooring!!!")
+    if(R > 180):
+        R = 180
+        print("R capping!!!")
+    elif(R < 0):
+        R = 0
+        print("R flooring!!!")
 
-def iloc(a,b,c): #inverse law of cosines
-    return(round(math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b))),6))
-
-def cbt():
-    return(math.sqrt((dist - clawLength)**2 + (z - baseHeight)**2))
-
-def fA():
-    A1 = math.degrees(math.tan((z-baseHeight)/(dist-clawLength)))
-    A2 = iloc(cbt(),arm1,arm2)
-    if(baseHeight > z):
-        A = A2 - A1
-    elif(baseHeight < z):
-        A = A2 + A1
+def cn(): #calculate numbers
+    global R
+    try:
+        deg = round(math.degrees(math.atan(y/x)))
+    except ZeroDivisionError:
+        deg = 0
+    if(deg > 0):
+        R = deg
+    elif(deg < 0):
+        R = (180 + deg)
     else:
-        A = A2
-    return(A)
+        R = 90
+    global pdist
+    pdist = round(math.sqrt(x**2 + y**2),6)
+    global dist
+    dist = round(math.sqrt((pdist - clawLength)**2 + (z - baseHeight)**2),6)
+    global A
+    A1 = math.degrees(math.atan(((z-baseHeight)/(pdist-clawLength))))
+    A2 = iloc(dist,arm1,arm2)
+    A = round((A1 + A2),2)
+    global C
+    C = iloc(arm1,arm2,dist)
+    global B
+    B = A + C - 90
 
-def calc(x,y,z):
-    dist = round(math.sqrt(x**2 + y**2),6)
-    print(dist)
-    C = iloc(arm1,arm2,cbt())
-    A = fA()
-    B = C + A
-    print(C,A,B)
-
-calc(30,70,30)
-
-'''print("Distance =",dist)
-print("c =",cbt())
-print("Angle A =",fA())
-print("Angle C =",iloc(arm1,arm2,cbt()))'''
+while True:
+    smooth()
+    cn()
+    cv()
+    print("R =",R)
+    print("A =",A)
+    print("B =",B)
+    print("C =",iloc(arm1,arm2,dist))
+    print("pdist =",pdist)
+    print("dist =",dist)
+    kit.servo[0].angle = R
+    kit.servo[1].angle = A
+    kit.servo[2].angle = 180 - (C - 45)
+    kit.servo[13].angle = B
+    sleep(1/40) # 1/40th of a second
